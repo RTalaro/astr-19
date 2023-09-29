4b34dbb2704cb4b510f9588749bc82864a45cb44
RTalaro <146395601+RTalaro@users.noreply.github.com>
2023-09-29 14:49:12 -0700

    Session 1 prompt from Coding Journal 1

--- cj1-s1.py
+++ cj1-s1.py
@@ -1,3 +1,1 @@
-def main():
-	print('My name is Reece Talaro. My favorite movie trilogy is Rush Hour. My favorite food is chicken katsu.'):
-	
+print('My name is Reece Talaro. My favorite movie trilogy is Rush Hour. My favorite food is chicken katsu.')
