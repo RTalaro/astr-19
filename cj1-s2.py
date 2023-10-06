df9c6193cb386c23284ffe31d5b16d087b810f7f
RTalaro <146395601+RTalaro@users.noreply.github.com>
2023-10-06 16:32:13 -0700

    Session 2 of Coding Journal 1

--- cj1-s2.py
+++ cj1-s2.py
@@ -1,11 +1,28 @@
-a = 5.0
-b = 6.0
-c = b+a
-print(c)
-print(type(c))
-d = 10
-e = 5
-print(d-e)
-print(type(d-e))
-print(b*e)
-print(type(b*e))
+def summ():
+	a = 5.0
+	b = 6.0
+	c = b+a
+	print(c)
+	print(type(c))
+	#adding floats and printing the type of the sum
+
+def difference():
+	d = 10
+	e = 5
+	print(d-e)
+	print(type(d-e))
+
+def product():
+	b = 6.0
+	e = 5
+	print(b*e)
+	print(type(b*e))
+
+def main():
+	summ()
+	difference()
+	product()
+	#calls all three functions
+
+if __name__ == "__main__":
+	main()
