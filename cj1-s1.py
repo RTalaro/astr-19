cd78a196d05a1ca5ee9956d40e4ba5526ee88dff
RTalaro <146395601+RTalaro@users.noreply.github.com>
2023-10-06 16:31:56 -0700

    Session 1 of Coding Journal 1

--- cj1-s1.py
+++ cj1-s1.py
@@ -1,1 +1,20 @@
-print('My name is Reece Talaro. My favorite movie trilogy is Rush Hour. My favorite food is chicken katsu.')
+def name():
+	print('My name is Reece Talaro.')
+	#prints this sentence in text
+
+def movie():
+	print('My favorite movie trilogy is Rush Hour.')
+	#prints this sentence in text
+
+def food():
+	print('My favorite food is chicken katsu.')
+	#prints this sentence in text
+
+def main():
+	name()
+	movie()
+	food()
+	#calls all three functions
+
+if __name__ == "__main__":
+	main()
