300a62f61285df6acf3e7486625a0920d8e25c57
RTalaro <146395601+RTalaro@users.noreply.github.com>
2023-10-19 01:00:34 -0700

    Session 3 of Coding Journal 1

---
+++ cj1-s3.py
@@ -0,0 +1,8 @@
+def f(x): #defines f(x) as function
+	result=(x**3)+8 #calculates
+	print(result) #prints result
+	if result>27: #tests if result is greater than 27
+		print('YAY!') #prints 'YAY!' if true
+
+if __name__ == '__main__':
+	f(9) #calls f(x) using 9
