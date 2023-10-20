e5f095976d4fba32047409003b795845a35f8ebb
RTalaro <146395601+RTalaro@users.noreply.github.com>
2023-10-20 02:20:37 -0700

    Session 5 of Coding Journal 1

---
+++ cj1-s5.py
@@ -0,0 +1,15 @@
+import math as m
+import numpy as np
+from astropy.table import Table
+from astropy.io import ascii
+
+def main():
+	x=np.linspace(0,2*m.pi,1000)
+	y=m.sin(x)
+	data=Table([x,y],names=['sin(x)','x'])
+	ascii.write(data,'table.txt',format='commented_header')
+	data_in=ascii.read('table.txt')
+	print(data_in)
+
+if __name__=='__main__':
+	main()
