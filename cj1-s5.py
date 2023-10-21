013799a51ce8dcf11c143243c6d2e0973ada4df0
RTalaro <146395601+RTalaro@users.noreply.github.com>
2023-10-20 17:01:54 -0700

    Session 5 of Coding Journal 1

--- cj1-s5.py
+++ cj1-s5.py
@@ -6,7 +6,7 @@ from astropy.io import ascii
 def main():
 	x=np.linspace(0,2*m.pi,1000)
 	y=m.sin(x)
-	data=Table([x,y],names=['sin(x)','x'])
+	data=Table([y,x],names=['sin(x)','x'])
 	ascii.write(data,'table.txt',format='commented_header')
 	data_in=ascii.read('table.txt')
 	print(data_in)
